3ef241e490096cdffe2c376c9cd02b4a5e67347e
Cesar Espinosa <cesarjr.espinosa@gmail.com>
2015-09-29 12:26:11 -0500

    Second Version for Recovery with the htaccess, update with http://www.changemakerday.com/

--- assets/php/chmday.php
+++ assets/php/chmday.php
@@ -19,23 +19,24 @@ class ChangeMaker{
 	//static public $db_user 		= "";
 	//static public $db_password	= "";
 
+	//static public $domain = "http://localhost/changemaker/";
 	static public $domain = "http://www.changemakerday.com/";
 
 	static function menubar($logo){
 		echo
 			'<div id="menu-bar">';
 		if($logo){
-			echo'<a href="'.self::$domain.'"><img src="'.self::$domain.'assets/img/logo-naranja.svg"></a>';
+			echo'<a href="http://www.changemakerday.com/"><img src="http://www.changemakerday.com/assets/img/logo-naranja.svg"></a>';
 		}else{
-			echo'<a href="'.self::$domain.'"><img src="'.self::$domain.'assets/img/logo-negro.svg"></a>';	
+			echo'<a href="http://www.changemakerday.com/"><img src="http://www.changemakerday.com/assets/img/logo-negro.svg"></a>';	
 		}
 		echo
 				'<div id="menu-bar-options">
-					<a id="program-bar" href="'.self::$domain.'programa/">PROGRAMA</a>
-					<a id="press-bar" href="'.self::$domain.'prensa/">PRENSA</a>
-					<a id="hackathon-bar" href="'.self::$domain.'socialhack/">HACKATHON</a>
+					<a id="program-bar" href="http://www.changemakerday.com/programa/">PROGRAMA</a>
+					<a id="press-bar" href="http://www.changemakerday.com/prensa/">PRENSA</a>
+					<a id="hackathon-bar" href="http://www.changemakerday.com/socialhack/">HACKATHON</a>
 					<p>HULT PRICE</p>
-					<a id="contact-bar" href="'.self::$domain.'contacto/">CONTACTO</a>
+					<a id="contact-bar" href="http://www.changemakerday.com/contacto/">CONTACTO</a>
 				</div>
 			</div>
 		';
--- index.php
+++ index.php
@@ -7,18 +7,18 @@
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8" /> 
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 		<title>ChangeMaker Day | Inicio</title>
-		<link rel="stylesheet" type="text/css" href="<?php echo ChangeMaker::$domain; ?>assets/css/bootstrap.min.css"/>
-		<link rel="stylesheet" type="text/css" href="<?php echo ChangeMaker::$domain; ?>assets/css/dimmer.min.css"/>
-		<link rel="stylesheet" type="text/css" href="<?php echo ChangeMaker::$domain; ?>assets/css/shape.min.css"/>
-		<link rel="stylesheet" type="text/css" href="<?php echo ChangeMaker::$domain; ?>assets/css/chmday.css">
-		<link rel="stylesheet" type="text/css" href="<?php echo ChangeMaker::$domain; ?>assets/css/main.css">
+		<link rel="stylesheet" type="text/css" href="http://www.changemakerday.com/assets/css/bootstrap.min.css"/>
+		<link rel="stylesheet" type="text/css" href="http://www.changemakerday.com/assets/css/dimmer.min.css"/>
+		<link rel="stylesheet" type="text/css" href="http://www.changemakerday.com/assets/css/shape.min.css"/>
+		<link rel="stylesheet" type="text/css" href="http://www.changemakerday.com/assets/css/chmday.css">
+		<link rel="stylesheet" type="text/css" href="http://www.changemakerday.com/assets/css/main.css">
 		<!-- Icono de la pagina -->
-		<link rel="shorcut icon" href="<?php echo ChangeMaker::$domain; ?>assets/img/icon.ico">
-		<script type="text/javascript" src="<?php echo ChangeMaker::$domain; ?>assets/js/jquery.min.js"></script>
-		<script type="text/javascript" src="<?php echo ChangeMaker::$domain; ?>assets/js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="<?php echo ChangeMaker::$domain; ?>assets/js/main.js"></script>
-		<script type="text/javascript" src="<?php echo ChangeMaker::$domain; ?>assets/js/dimmer.min.js"></script>
-		<script type="text/javascript" src="<?php echo ChangeMaker::$domain; ?>assets/js/shape.min.js"></script>
+		<link rel="shorcut icon" href="http://www.changemakerday.com/assets/img/icon.ico">
+		<script type="text/javascript" src="http://www.changemakerday.com/assets/js/jquery.min.js"></script>
+		<script type="text/javascript" src="http://www.changemakerday.com/assets/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="http://www.changemakerday.com/assets/js/main.js"></script>
+		<script type="text/javascript" src="http://www.changemakerday.com/assets/js/dimmer.min.js"></script>
+		<script type="text/javascript" src="http://www.changemakerday.com/assets/js/shape.min.js"></script>
 	</head>
 	<body>
 
@@ -30,14 +30,14 @@
 		<!-- Pantallazo con parallax, no tocar :)-->
 		<div id="main">
 			<ul id="scene">
-				<li class="layer" data-depth="0.10"><img src="<?php echo ChangeMaker::$domain; ?>assets/img/parallax/slogan.png"></li>
-			  	<li class="layer" data-depth="0.50"><img src="<?php echo ChangeMaker::$domain; ?>assets/img/parallax/parte1.png"></li>
-			  	<li class="layer" data-depth="0.30"><img src="<?php echo ChangeMaker::$domain; ?>assets/img/parallax/parte2.png"></li>
-			  	<li class="layer" data-depth="0.70"><img src="<?php echo ChangeMaker::$domain; ?>assets/img/parallax/parte3.png"></li>
-			  	<li class="layer" data-depth="0.80"><img src="<?php echo ChangeMaker::$domain; ?>assets/img/parallax/parte4.png"></li>
-			  	<li class="layer" data-depth="0.99"><img src="<?php echo ChangeMaker::$domain; ?>assets/img/parallax/parte5.png"></li>
-			  	<li class="layer" data-depth="0.10"><img src="<?php echo ChangeMaker::$domain; ?>assets/img/parallax/logo.png"></li>
-			  	<li class="layer" data-depth="0.00"><img src="<?php echo ChangeMaker::$domain; ?>assets/img/parallax/name.png"></li>
+				<li class="layer" data-depth="0.10"><img src="http://www.changemakerday.com/assets/img/parallax/slogan.png"></li>
+			  	<li class="layer" data-depth="0.50"><img src="http://www.changemakerday.com/assets/img/parallax/parte1.png"></li>
+			  	<li class="layer" data-depth="0.30"><img src="http://www.changemakerday.com/assets/img/parallax/parte2.png"></li>
+			  	<li class="layer" data-depth="0.70"><img src="http://www.changemakerday.com/assets/img/parallax/parte3.png"></li>
+			  	<li class="layer" data-depth="0.80"><img src="http://www.changemakerday.com/assets/img/parallax/parte4.png"></li>
+			  	<li class="layer" data-depth="0.99"><img src="http://www.changemakerday.com/assets/img/parallax/parte5.png"></li>
+			  	<li class="layer" data-depth="0.10"><img src="http://www.changemakerday.com/assets/img/parallax/logo.png"></li>
+			  	<li class="layer" data-depth="0.00"><img src="http://www.changemakerday.com/assets/img/parallax/name.png"></li>
 			  </ul>
 		</div>
 
@@ -49,16 +49,16 @@
 
 		<div id="about">
 			<div id="about-info" class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-				<img src="<?php echo ChangeMaker::$domain; ?>assets/img/que-es.svg"/>
+				<img src="http://www.changemakerday.com/assets/img/que-es.svg"/>
 				<p>Es la celebración de la innovación y el emprendimiento social, en la que la comunidad del 
 				campus tiene la oportunidad de acercarse a las problemáticas sociales, compartir puntos de vista y 
 				descubrir nuevas herramientas para la transformación.<br>
 				Es un espacio de encuentro y diálogo cuyo objetivo es fortalecer el ecosistema de líderes con 
 				espíritu emprendedor y sentido humano, es decir, agentes de cambio.</p>
-				<img id="ashoka-logo" src="<?php echo ChangeMaker::$domain; ?>assets/img/ashoka-campus.svg"/>
+				<img id="ashoka-logo" src="http://www.changemakerday.com/assets/img/ashoka-campus.svg"/>
 			</div>
 			<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-				<img id="about-dots"src="<?php echo ChangeMaker::$domain; ?>assets/img/about.svg"/>
+				<img id="about-dots"src="http://www.changemakerday.com/assets/img/about.svg"/>
 			</div>	
 		</div>
 		
@@ -66,32 +66,32 @@
 		</div>-->
 
 		<div id="speakers">
-			<img id="speakers-dots" src="<?php echo ChangeMaker::$domain; ?>assets/img/speakers-dots.svg"/>
-			<img id="speakers-prox" src="<?php echo ChangeMaker::$domain; ?>assets/img/speakers-prox.png">
+			<img id="speakers-dots" src="http://www.changemakerday.com/assets/img/speakers-dots.svg"/>
+			<img id="speakers-prox" src="http://www.changemakerday.com/assets/img/speakers-prox.png">
 		</div>
 
 		<!--<div id="inter-2">
 		</div>-->
 
 		<div id="sponsors">
-			<img id="sponsors-chmday" src="<?php echo ChangeMaker::$domain; ?>assets/img/sponsors.svg"/>
+			<img id="sponsors-chmday" src="http://www.changemakerday.com/assets/img/sponsors.svg"/>
 			<div id="sponsors-logos">
 				<div class="row">
-					<div class="col-xs-12 col-sm-12 col-md-3 col-lg-6"><img src="<?php echo ChangeMaker::$domain; ?>assets/img/sponsors/tec-azul.svg"/></div>
-					<div class="col-xs-12 col-sm-12 col-md-3 col-lg-6"><img src="<?php echo ChangeMaker::$domain; ?>assets/img/sponsors/ashoka-campus.svg"/></div>
+					<div class="col-xs-12 col-sm-12 col-md-3 col-lg-6"><img src="http://www.changemakerday.com/assets/img/sponsors/tec-azul.svg"/></div>
+					<div class="col-xs-12 col-sm-12 col-md-3 col-lg-6"><img src="http://www.changemakerday.com/assets/img/sponsors/ashoka-campus.svg"/></div>
 				</div>
 				<div class="row">
 					<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3"></div>
-					<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3"><img src="<?php echo ChangeMaker::$domain; ?>assets/img/sponsors/zona-ei.svg"/></div>
-					<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3"><img src="<?php echo ChangeMaker::$domain; ?>assets/img/sponsors/acm.svg"/></div>
+					<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3"><img src="http://www.changemakerday.com/assets/img/sponsors/zona-ei.svg"/></div>
+					<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3"><img src="http://www.changemakerday.com/assets/img/sponsors/acm.svg"/></div>
 					<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3"></div>
 				</div>
 			</div>
-			<img id="sponsors-dots" src="<?php echo ChangeMaker::$domain; ?>assets/img/sponsors-dots.svg"/>
+			<img id="sponsors-dots" src="http://www.changemakerday.com/assets/img/sponsors-dots.svg"/>
 		</div>
 
 		<!-- Script para que funcione parallax, no tocar :)-->
-		<script type="text/javascript" src="<?php echo ChangeMaker::$domain; ?>assets/js/parallax.js"></script>
+		<script type="text/javascript" src="http://www.changemakerday.com/assets/js/parallax.js"></script>
 		<script type="text/javascript">
 			var scene = document.getElementById('scene');
 			var parallax = new Parallax(scene);
