e6a020c7a355565759c9fa4e7afda9f3eee6c538
Cesar Espinosa <cesarjr.espinosa@gmail.com>
2015-10-04 14:26:14 -0500

    7th version, map

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,4 +1,22 @@
+var domain = 'http://www.changemakerday.com/';
 $(document).ready(function(){
     
     $('#contact-bar').toggleClass('active-bar');
+
+    $('#scenario-1').click(function(event){
+    	src1 = domain+"assets/img/map-1.png";
+    	$('#map').attr("src", src1);
+    });
+     $('#scenario-2').click(function(event){
+    	src1 = domain+"assets/img/map-2.png";
+    	$('#map').attr("src", src1);
+    });
+      $('#scenario-3').click(function(event){
+    	src1 = domain+"assets/img/map-3.png";
+    	$('#map').attr("src", src1);
+    });
+       $('#scenario-4').click(function(event){
+    	src1 = domain+"assets/img/map-4.png";
+    	$('#map').attr("src", src1);
+    });
 });
